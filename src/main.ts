c0e3c7dc44c86c7cd91f3d223bd61f3555ec5210
Philip Smolen <philip@trade-ideas.com>
2025-11-01 01:08:29 -0700

    Bug fix:  Overlapping objects.
    Within a cube I was displaying things properly.
    It was using "3D depth sorting".
    
    Between cubes it was relying on the z-order,
    the "painter's algorithm",
    which is often wrong.
    
    transform-style: preserve-3d; seemed to fix the problem.
    Grok suggested the other changes to be safe.

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ masterCube.remove();
 
 for (let z = -5; z < 0; z++) {
   const translateZ = `${z * 6}lh`;
-  for (let x = 0; x < 5; x++) {
+  for (let x = 0; x < 9; x++) {
     const translateX = `${x * 6}lh`;
     for (let y = 0; y < 5; y++) {
       const translateY = `${y * 6}lh`;
@@ -20,12 +20,6 @@ for (let z = -5; z < 0; z++) {
 }
 
 const container = getById("container1", HTMLDivElement);
-/*
-let current:
-  | undefined
-  | { x: number; y: number; lastUpdate: DOMHighResTimeStamp };
-let desired: undefined | { x: number; y: number };
-*/
 
 document.addEventListener("mousemove", (event: MouseEvent) => {
   const rect = container.getBoundingClientRect();
@@ -34,34 +28,4 @@ document.addEventListener("mousemove", (event: MouseEvent) => {
   const x = event.clientX - rect.left;
   const y = event.clientY - rect.top;
   container.style.perspectiveOrigin = `${x}px ${y}px`;
-  //desired = { x, y };
 });
-
-/*
-const animationLoop = new AnimationLoop((time) => {
-  const halfLife = 150;
-  if (desired) {
-    if (current) {
-      const timePassed = time - current.lastUpdate;
-      const decayFactor = Math.pow(0.5, timePassed / halfLife);
-      current.x = desired.x + (current.x - desired.x) * decayFactor;
-      current.y = desired.y + (current.y - desired.y) * decayFactor;
-      current.lastUpdate = time;
-    } else {
-      current = { ...desired, lastUpdate: time };
-    }
-    container.style.perspectiveOrigin = `${current.x}px ${current.y}px`;
-  }
-});
-(window as any).animationLoop = animationLoop;
-*/
-
-/**
- * z order is always my problem.
- * I.e. drawing one object on top of another.
- *  Right?
- * I thought I saw an example that said otherwise.
- * Stacking context?
- * The thing that says to preserve the 3d.
- * https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Properties/transform-style
- */
