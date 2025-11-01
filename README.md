43235b463e2b30e962bd29305bed0e2c35634e69
Philip Smolen <philip@trade-ideas.com>
2025-11-01 15:33:38 -0700

    Add the spinning animation.
    Click on a cube and it will start spinning.
    Then each of the cubes behind it will start spinning in turn.
    (Because you can only click on the ones in the front.)
    The transform origin is now the center of cube in all three dimensions.
    
    Add help and README.
    
    Adjust the perspective so its not so extreme.

---
+++ README.md
@@ -0,0 +1,19 @@
+# 3D Perspective in CSS
+
+All of the transform are done in CSS.
+
+The sides of the cubes are `div`’s.
+I use transforms like `rotateY()` and `translateZ()` to move the  `div`’s where I want them in 3D space.
+And I use a couple more CSS properties to adjust the camera.
+The rest is automatic.
+
+I only create rectangles.
+CSS turns those into trapezoids.
+
+## How to Use
+
+Go to https://tradeideasphilip.github.io/css-3d-perspective/.
+Move your mouse to adjust the vanishing point.
+Click on a cube to make it spin.
+
+More: https://youtu.be/c_eofk-gSRg.
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
-import { assertClass, assertNonNullable } from "phil-lib/misc";
+import {
+  assertClass,
+  assertNonNullable,
+  initializedArray,
+} from "phil-lib/misc";
 import "./style.css";
 import { AnimationLoop, getById, querySelector } from "phil-lib/client-misc";
 
@@ -6,18 +10,74 @@ const masterCube = querySelector(".cube", HTMLDivElement);
 const parent = assertNonNullable(masterCube.parentElement);
 masterCube.remove();
 
-for (let z = -5; z < 0; z++) {
-  const translateZ = `${z * 6}lh`;
-  for (let x = 0; x < 9; x++) {
+/**
+ * x ranges from 0 to width - 1.
+ */
+const width = 9;
+
+/**
+ * y ranges from 0 to height - 1.
+ */
+const height = 5;
+
+/**
+ * z ranges from 0 to depth - 1
+ */
+const depth = 5;
+
+const allCubes = initializedArray(width, () =>
+  initializedArray(height, () =>
+    initializedArray(depth, () => null as any as HTMLDivElement)
+  )
+);
+
+for (let z = 0; z < depth; z++) {
+  const translateZ = `${(z - depth) * 6}lh`;
+  for (let x = 0; x < width; x++) {
     const translateX = `${x * 6}lh`;
-    for (let y = 0; y < 5; y++) {
+    for (let y = 0; y < height; y++) {
       const translateY = `${y * 6}lh`;
       const clone = assertClass(masterCube.cloneNode(true), HTMLDivElement);
-      clone.style.transform = `translateX(${translateX}) translateY(${translateY}) translateZ(${translateZ})`;
+      clone.style.transform = `translateX(${translateX}) translateY(${translateY}) translateZ(${translateZ}) rotateY(var(--rotation))`;
       parent.append(clone);
+      allCubes[x][y][z] = clone;
     }
   }
 }
+
+allCubes.forEach((allOneX, x) => {
+  allOneX.forEach((xAndYFixed, y) => {
+    xAndYFixed.forEach((div, z) => {
+      if (!div) {
+        throw new Error(`Missing at (${x}, ${y}, ${z})`);
+      }
+    });
+  });
+});
+
+(window as any).allCubes = allCubes;
+
+allCubes.forEach((xFixed, x) => {
+  xFixed.forEach((xAndYFixed, y) => {
+    /**
+     * All elements at the current x and y.
+     * Sorted so the first one is the one closest to us.
+     */
+    const inOrder = xAndYFixed.toReversed();
+    inOrder[0].addEventListener("click", () => {
+      inOrder.forEach((cube, index) => {
+        cube.getAnimations().forEach((animation) => {
+          animation.cancel();
+        });
+        cube.animate([{ "--rotation": "0deg" }, { "--rotation": "360deg" }], {
+          duration: 4000,
+          // easing: "cubic-bezier(0.7, -0.67, 0.33, 1.66)",
+          delay: 1500 * index,
+        });
+      });
+    });
+  });
+});
 
 const container = getById("container1", HTMLDivElement);
 
