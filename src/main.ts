7d65743fe0c22347235e4087a63c13d08803336a
Philip Smolen <philip@trade-ideas.com>
2025-10-31 23:50:19 -0700

    More cubes.
    And when you move, add a bit of an exponential delay.
    
    And change the build setup.
    Copy from perspective-assistant.

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import { assertClass, assertNonNullable } from "phil-lib/misc";
 import "./style.css";
-import { getById, querySelector } from "phil-lib/client-misc";
+import { AnimationLoop, getById, querySelector } from "phil-lib/client-misc";
 
 const masterCube = querySelector(".cube", HTMLDivElement);
 const parent = assertNonNullable(masterCube.parentElement);
 masterCube.remove();
 
-for (let z = -5; z < 0; z++) {
+for (let z = -7; z < 0; z++) {
   const translateZ = `${z * 6}lh`;
-  for (let x = 0; x < 5; x++) {
+  for (let x = 0; x < 7; x++) {
     const translateX = `${x * 6}lh`;
-    for (let y = 0; y < 5; y++) {
+    for (let y = 0; y < 7; y++) {
       const translateY = `${y * 6}lh`;
       const clone = assertClass(masterCube.cloneNode(true), HTMLDivElement);
       clone.style.transform = `translateX(${translateX}) translateY(${translateY}) translateZ(${translateZ})`;
@@ -20,9 +20,10 @@ for (let z = -5; z < 0; z++) {
 }
 
 const container = getById("container1", HTMLDivElement);
-let current : undefined| {x:number, y:number, lastUpdate:DOMHighResTimeStamp};
-let desired : undefined| {x:number, y:number};
-
+let current:
+  | undefined
+  | { x: number; y: number; lastUpdate: DOMHighResTimeStamp };
+let desired: undefined | { x: number; y: number };
 
 document.addEventListener("mousemove", (event: MouseEvent) => {
   const rect = container.getBoundingClientRect();
@@ -30,10 +31,25 @@ document.addEventListener("mousemove", (event: MouseEvent) => {
   // Calculate the mouse position relative to the container
   const x = event.clientX - rect.left;
   const y = event.clientY - rect.top;
-
-  // Set perspective-origin to the point directly under the mouse
-  container.style.perspectiveOrigin = `${x}px ${y}px`;
+  desired = { x, y };
 });
+
+const animationLoop = new AnimationLoop((time) => {
+  const halfLife = 150;
+  if (desired) {
+    if (current) {
+      const timePassed = time - current.lastUpdate;
+      const decayFactor = Math.pow(0.5, timePassed / halfLife);
+      current.x = desired.x + (current.x - desired.x) * decayFactor;
+      current.y = desired.y + (current.y - desired.y) * decayFactor;
+      current.lastUpdate = time;
+    } else {
+      current = { ...desired, lastUpdate: time };
+    }
+    container.style.perspectiveOrigin = `${current.x}px ${current.y}px`;
+  }
+});
+(window as any).animationLoop = animationLoop;
 
 /**
  * z order is always my problem.
