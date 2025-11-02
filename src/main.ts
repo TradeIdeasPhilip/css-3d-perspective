19aa8497023634b46ec5ca28f43889365dd66172
Philip Smolen <philip@trade-ideas.com>
2025-11-02 12:55:19 -0800

    Add an option to rotate the entire stage 45°.
    See the new radio buttons on the bottom left of the page..

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import {
   initializedArray,
 } from "phil-lib/misc";
 import "./style.css";
-import { AnimationLoop, getById, querySelector } from "phil-lib/client-misc";
+import { getById, querySelector, querySelectorAll } from "phil-lib/client-misc";
 
 const masterCube = querySelector(".cube", HTMLDivElement);
 const parent = assertNonNullable(masterCube.parentElement);
@@ -23,7 +23,7 @@ const height = 5;
 /**
  * z ranges from 0 to depth - 1
  */
-const depth = 5;
+const depth = 9;
 
 const allCubes = initializedArray(width, () =>
   initializedArray(height, () =>
@@ -31,18 +31,64 @@ const allCubes = initializedArray(width, () =>
   )
 );
 
+const sideLength = 3;
+const spaceBetween = 3;
+const period = sideLength + spaceBetween;
+
+const allTranslateZ = new Set<string>();
 for (let z = 0; z < depth; z++) {
-  const translateZ = `${(z - depth) * 6}lh`;
+  const translateZ = `${(z - depth) * period}lh`;
+  allTranslateZ.add(translateZ);
   for (let x = 0; x < width; x++) {
-    const translateX = `${x * 6}lh`;
+    const translateX = `${x * period}lh`;
     for (let y = 0; y < height; y++) {
-      const translateY = `${y * 6}lh`;
+      const translateY = `${y * period}lh`;
       const clone = assertClass(masterCube.cloneNode(true), HTMLDivElement);
       clone.style.transform = `translateX(${translateX}) translateY(${translateY}) translateZ(${translateZ}) rotateY(var(--rotation))`;
       parent.append(clone);
       allCubes[x][y][z] = clone;
     }
   }
+}
+console.log([...allTranslateZ]);
+// Current result: ['-48lh', '-42lh', '-36lh', '-30lh', '-24lh', '-18lh', '-12lh', '-6lh']
+//    transform: ;
+
+{
+  const stageDiv = getById("stage", HTMLDivElement);
+  const stageWidth = width * period - spaceBetween;
+  stageDiv.style.width = `${stageWidth}lh`;
+  const stageHeight = height * period - spaceBetween;
+  stageDiv.style.height = `${stageHeight}lh`;
+  // I don't have an exact formula for this.
+  // I got this by trial and error.
+  // This causes it to rotate around its center.
+  const doRotate = `translateZ(-30lh) rotateY(45deg) translateZ(30lh)`;
+  function updateRotateFromGUI() {
+    const choice = querySelector(
+      '[name="rotateStage"]:checked',
+      HTMLInputElement
+    ).value;
+    switch (choice) {
+      case "45": {
+        stageDiv.style.transform = doRotate;
+        break;
+      }
+      case "0": {
+        stageDiv.style.transform = "";
+        break;
+      }
+      default: {
+        throw new Error("wtf");
+      }
+    }
+  }
+  updateRotateFromGUI();
+  querySelectorAll('[name="rotateStage"]', HTMLInputElement).forEach(
+    (element) => {
+      element.addEventListener("input", updateRotateFromGUI);
+    }
+  );
 }
 
 allCubes.forEach((allOneX, x) => {
@@ -71,7 +117,7 @@ allCubes.forEach((xFixed, x) => {
         });
         cube.animate([{ "--rotation": "0deg" }, { "--rotation": "360deg" }], {
           duration: 4000,
-          // easing: "cubic-bezier(0.7, -0.67, 0.33, 1.66)",
+          easing: "cubic-bezier(0.7, -0.67, 0.33, 1.66)",
           delay: 1500 * index,
         });
       });
